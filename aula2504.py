4dd3dda71f74f1c484fd848bc51445bfa5a08402
thierry <thierrysouza.clesher@gmail.com>
2025-04-25 16:12:57 -0300

    Add files via upload

---
+++ aula2504.py
@@ -0,0 +1,23 @@
+#print ("olá")
+import pyautogui as robo
+robo. moveTo (700,1050)
+robo.leftClick(700,1050)
+robo.write("google")
+robo.press("enter")
+
+
+robo.leftClick(670,50)
+robo.leftClick(670,50)
+robo.write("youtube.com")
+robo.press("enter")
+
+robo.sleep (3)
+robo. moveTo (700,170)
+robo.leftClick(700,170)
+robo.leftClick(700,170)
+robo.write("vou dar choque no cu")
+robo.press("enter")
+
+robo.sleep (3)
+robo. moveTo (700,850)
+robo.leftClick(700,850)
